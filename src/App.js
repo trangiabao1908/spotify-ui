c1ad08f4fecb1279c345b207f178137d81dad6e2
trangiabao1908 <hieu40262@gmail.com>
2023-09-04 01:07:52 +0700

    Add GlobalStyles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Navbar from "./components/Navbar";
+import Navbar from './components/Navbar';
 function App() {
-  return (
-    <div className="App">
-      <Navbar></Navbar>
-    </div>
-  );
+   return (
+      <div className="App">
+         <Navbar></Navbar>
+      </div>
+   );
 }
 
 export default App;
